2f8e49fa2f1313c51123dde2e14256d4ae47a796
joshuaimmanuel-netizen <106328263+joshuaimmanuel-netizen@users.noreply.github.com>
2022-07-31 23:27:47 +0530

    Add files via upload
    .

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main{
+    public static void main(String[] args){
+        String a="Joshua immanuel";
+        String b="Joshua immanue";
+        boolean c=a.equals(b);
+        System.out.println(c);
+    }
+}
